b25e96009d7230ef620bc26af8b7514d2907dc8a
sa-tsuklog <mail2.sa.tsuklog@gmail.com>
2014-03-09 02:49:23 +0900

    ad7176-2 driver added

---
+++ src/AD7176-2/Seeker.hpp
@@ -0,0 +1,15 @@
+/*
+ * Seeker.hpp
+ *
+ *  Created on: 2014/03/08
+ *      Author: sa
+ */
+
+#ifndef SEEKER_HPP_
+#define SEEKER_HPP_
+
+
+void prvSeekerTask(void *pvParameters);
+void enqueAdData(float* data);
+
+#endif /* SEEKER_HPP_ */
