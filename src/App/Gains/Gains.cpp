6e8ef131777e3993801aeecdd92cbfb3508576c8
sa-tsuklog <mail2.sa.tsuklog@gmail.com>
2014-04-13 23:55:34 +0900

    Add App/Gains/Gains.cpp and related files

---
+++ src/App/Gains/Gains.cpp
@@ -0,0 +1,18 @@
+/*
+ * Gains.cpp
+ *
+ *  Created on: 2014/04/13
+ *      Author: sa
+ */
+
+#include "Gains.h"
+
+
+void Gains::prvGainsTask(void *pvParameters){
+	
+	
+	
+	while(1){
+		vTaskDelay(100);
+	}
+}
---
+++ src/App/Gains/Gains.h
@@ -0,0 +1,21 @@
+/*
+ * Gains.h
+ *
+ *  Created on: 2014/04/13
+ *      Author: sa
+ */
+
+#ifndef GAINS_H_
+#define GAINS_H_
+
+class Gains{
+private:
+public:
+	static void prvGainsTask(void *pvParameters);
+};
+
+
+
+#endif
+
+
